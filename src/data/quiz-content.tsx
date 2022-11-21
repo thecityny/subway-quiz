0eb6c2f5ed7f992e0d22cea1ed29995cfe121b36
Sam Rabiyah <sraby@users.noreply.github.com>
2022-11-21 15:03:32 -0500

    Add remaining quiz content

--- src/data/quiz-content.tsx
+++ src/data/quiz-content.tsx
@@ -8,22 +8,22 @@ export type QuestionContent = {
 
 export const getQuizContent: () => QuestionContent[] = () => [
   {
-    photo: require("../assets/1.jpg"),
+    photo: require("../assets/images/q1.jpg"),
     caption: <>At which station can you find these stylish signs?</>,
     correctAnswer: 636,
   },
   {
-    photo: require("../assets/2.jpg"),
+    photo: require("../assets/images/q2.jpg"),
     caption: <>Which stop is this at?</>,
-    correctAnswer: 622,
+    correctAnswer: 20,
   },
   {
-    photo: require("../assets/3.jpg"),
+    photo: require("../assets/images/q3.jpg"),
     caption: <>Which station are these tiles at?</>,
     correctAnswer: 609,
   },
   {
-    photo: require("../assets/4.jpg"),
+    photo: require("../assets/images/q4.jpg"),
     caption: (
       <>
         If you pay attention to exits you’ll have seen this one. Where is it at?
@@ -32,8 +32,51 @@ export const getQuizContent: () => QuestionContent[] = () => [
     correctAnswer: 164,
   },
   {
-    photo: require("../assets/5.jpg"),
+    photo: require("../assets/images/q5.jpg"),
     caption: <>At which subway station can you see this sign?</>,
     correctAnswer: 628,
+  },
+  {
+    photo: require("../assets/images/q6.jpg"),
+    caption: (
+      <>
+        Which subway station has these cool platform benches by @MTAArtsDesign?
+      </>
+    ),
+    correctAnswer: 604,
+  },
+  {
+    photo: require("../assets/images/q7.jpg"),
+    caption: (
+      <>
+        At which Manhattan subway hub can you find Sol Lewitt’s “Whirls and
+        twirls”?
+      </>
+    ),
+    correctAnswer: 614,
+  },
+  {
+    photo: require("../assets/images/q8.jpg"),
+    caption: <>Where is this? Hint: it’s the end of the line.</>,
+    correctAnswer: 447,
+  },
+  {
+    photo: require("../assets/images/q9.jpg"),
+    caption: <>Which subway stop is this?</>,
+    correctAnswer: 461,
+  },
+  {
+    photo: require("../assets/images/q10.jpg"),
+    caption: (
+      <>
+        One of the few subway stops located at ground level, which one is this?
+      </>
+    ),
+    correctAnswer: 436,
+  },
+  {
+    photo: require("../assets/images/q11.jpg"),
+    caption: <>Where in the system can you buy bitcoin?</>,
+    correctAnswer: 334,
   },
 ];
